39aad39b5db8b950574850d1754030d1ac9dda1d
Jojo <joseph.sharratt@gmail.com>
2016-02-24 16:01:36 +0000

    Fizzbuzz done

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,19 @@
 require './lib/fizzbuzz'
 
 describe 'fizzbuzz' do
+  it 'returns number when it\'s not divisible by 3 and 5' do
+    expect(fizzbuzz(4)).to eq 4
+  end
+
+  it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
+  end
+
+  it 'returns "fizzbuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
   end
 end
